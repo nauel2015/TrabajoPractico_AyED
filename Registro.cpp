1125045f306de60cb019abbfae9f0a3cf2f1da0f
Nauel2015 <nricheri@frba.utn.edu.ar>
2025-06-26 13:59:50 -0300

    Cargando archivos.

---
+++ Registro.cpp
@@ -0,0 +1,24 @@
+
+// Comision: K1153.
+// Profesor: Federico maidan.
+// Correo: fedemaidan@frba.utn.edu.ar
+
+// registros.cpp
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+
+// Declaracion de las funciones.
+
+int main()
+{
+
+
+
+}
+
+// Defincion de las funciones.
