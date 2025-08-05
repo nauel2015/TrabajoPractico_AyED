5589e37a3c8669fc608742ced567ee56eab3be6a
Matias Santo Stefano <msantostefano@frba.utn.edu.ar>
2025-08-05 20:35:26 -0300

    ordenamiento + mostrar ranking

--- Analisis.cpp
+++ Analisis.cpp
@@ -252,7 +252,26 @@ void rankingProductosMasVendidos()
     fclose(archVentas);
 
     //Ordenar el array de prod x cantidad de mayor a menor
-    // ...
+    for (int i = 0; i < lenProductos - 1; i++)
+    {
+        for (int j = i + 1; j < lenProductos; j++)
+        {
+            if (productos[j].cantidad > productos[i].cantidad)
+            {
+             ProductoContador aux = productos[i];
+             productos[i] = productos[j];
+             productos[j] = aux;
+            }
+        }
+        
+    }
+    
     //Mostrar Ranking
-    // ...
+    cout << "Ranking de productos más vendidos: " << endl;
+    for (int i = 0; i < lenProductos; i++)
+    {
+        cout << "Producto " << productos[i].codigoProducto 
+        << " - Vendido " << productos[i].cantidad << " veces " << endl;
+    }
+    
 }
